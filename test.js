1f5107dfa855b18fde18702b6086d0a768384fe2
FukushiKen2 <73925597+FukushiKen2@users.noreply.github.com>
2020-11-26 00:35:56 +0900

    Create test.js

---
+++ test.js
@@ -0,0 +1,26 @@
+function ohayou() {
+  var a = Math.floor(Math.random()*(4));
+
+if(a == 0){
+alert("イキスギィ！");
+}
+
+if(a == 1){
+alert("ああ〜いいっすねえ〜");
+}
+
+if(a == 2){
+alert("まーだ時間掛かりそうですかね～？");
+}
+
+if(a == 3){
+alert("見たけりゃ見せてやるよ（震え声）");
+}
+
+if(a == 4){
+alert("２４歳，学生です");
+
+}
+
+}
+~                   
